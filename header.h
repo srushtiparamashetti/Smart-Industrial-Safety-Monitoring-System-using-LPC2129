7aa3dfe766b9dd0a260ed438a2a83df038127779
srushtiparamashetti <paramashettichittu0703@gmail.com>
2026-09-18 18:48:19 +0530

    Create header.h

---
+++ header.h
@@ -0,0 +1,16 @@
+extern void delay_sec(unsigned int sec);
+extern void delay_ms(unsigned int ms);
+extern void lcd_data(unsigned char data);
+extern void lcd_cmd(unsigned int cmd);
+extern void lcd_init(void);
+extern void lcd_string(char *ptr);
+extern void lcd_integer(int num);
+extern void lcd_float(float f);
+extern void uart0_init(unsigned int boud);
+extern void uart0_tx(unsigned char data);
+extern unsigned char uart0_rx(void);
+extern void uart0_tx_string(char *ptr);
+extern void uart0_integer(int num);
+extern void uart0_float(float f);
+extern void adc_init(void);
+extern unsigned int adc_read(unsigned int ch_num);
